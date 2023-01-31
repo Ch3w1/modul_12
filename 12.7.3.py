adb8a70c77e4eb3135a1c61740d35eb69e3d8f7d
Ch3w1 <122768742+Ch3w1@users.noreply.github.com>
2023-01-31 17:49:02 +0300

    Add files via upload

---
+++ 12.7.3.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("введите сумму:"))
+L = []
+for x in per_cent: L.append(int(per_cent[x] * money / 100))
+max_L = max(L)
+print("deposit=" + str(L))
+print("Максимальная сумма которую вы можете заработать:" + str(max_L))
