4d4a15b281f8331cfbb7abb692dc137e7ec63d94
Ch3w1 <erohin.nick3@gmail.com>
2023-02-02 21:42:56 +0300

    12.7.3.py

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,7 +1,7 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("введите сумму:"))
-L = []
-for x in per_cent: L.append(int(per_cent[x] * money / 100))
-max_deposit = max(L)
-print("deposit=" + str(L))
+deposit = []
+for x in per_cent: deposit.append(int(per_cent[x] * money / 100))
+max_deposit = max(deposit)
+print("deposit=" + str(deposit))
 print("Максимальная сумма которую вы можете заработать:" + str(max_deposit))
